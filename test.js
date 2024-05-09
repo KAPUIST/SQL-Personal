f0ad60dc00cebae0da542bf12e843a8e77c2506e
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-05-10 08:54:23 +0900

    Algo

---
+++ test.js
@@ -0,0 +1,13 @@
+const teamSixthSense = {
+  teamName: 식스센스,
+  project: {
+    name: MOVOLLEH,
+    description: '5명이 시너지를 내어 열심히 만들었습니다!,',
+  },
+  members: 5,
+  technology: ['HTML', 'CSS', 'JavaScript'],
+  greet() {
+    console.log(`안녕하세요, ${this.teamName} 입니다`);
+  },
+};
+teamSixthSense.greet();
